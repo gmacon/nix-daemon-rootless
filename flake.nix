e8d3d83f669e95f3c6f53e29d7c0c972227c43a2
George Macon <george@themacons.net>
2025-04-11 18:24:40 -0400

    With --store local
    Based on a suggestion from Robert Hensing [1], add --store local to the
    nix-daemon invocation. This revealed further issues that I chased for a
    little while. I'm now running into issues with the store that's mounted
    into the VM being read-only, which wouldn't happen on a full machine,
    but I'll worry about that later.
    
    [1]: https://functional.cafe/@roberth/114319211674977879

--- flake.nix
+++ flake.nix
@@ -5,44 +5,61 @@
       nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          {
-            system.stateVersion = "24.11";
+          (
+            { config, pkgs, ... }:
+            let
+              preparePaths = pkgs.writeShellScript "prepare-paths" ''
+                if [ "$(${pkgs.coreutils}/bin/stat --format %U /nix)" = "root" ]; then
+                  ${pkgs.coreutils}/bin/chown --recursive nix /nix
+                fi
+              '';
+            in
+            {
+              system.stateVersion = "24.11";
 
-            nix = {
-              settings = {
-                experimental-features = [
-                  "auto-allocate-uids"
-                  "flakes"
-                  "nix-command"
-                ];
-                auto-allocate-uids = true;
+              boot.readOnlyNixStore = false;
+
+              nix = {
+                settings = {
+                  experimental-features = [
+                    "auto-allocate-uids"
+                    "flakes"
+                    "nix-command"
+                  ];
+                  auto-allocate-uids = true;
+                };
               };
-            };
 
-            systemd.services.nix-daemon = {
-              serviceConfig = {
-                User = "nix";
-                AmbientCapabilities = "CAP_DAC_READ_SEARCH";
+              systemd.services.nix-daemon = {
+                serviceConfig = {
+                  User = "nix";
+                  AmbientCapabilities = "CAP_DAC_READ_SEARCH";
+                  ExecStartPre = "+${preparePaths}";
+                  ExecStart = [
+                    ""
+                    "@${config.nix.package}/bin/nix-daemon nix-daemon --daemon --store local"
+                  ];
+                  RuntimeDirectory = "nix-daemon";
+                  StateDirectory = "nix-daemon";
+                  CacheDirectory = "nix-daemon";
+                  LogsDirectory = "nix-daemon";
+                };
               };
-            };
 
-            systemd.tmpfiles.rules = [
-              "d /nix/var/nix/daemon-socket 0755 nix root - -"
-            ];
+              users.users.nix = {
+                isSystemUser = true;
+                group = "nixbld";
+              };
 
-            users.users.nix = {
-              isSystemUser = true;
-              group = "nixbld";
-            };
+              users.users.a = {
+                isNormalUser = true;
+                group = "wheel";
+                password = "a";
+              };
 
-            users.users.a = {
-              isNormalUser = true;
-              group = "wheel";
-              password = "a";
-            };
-
-            security.sudo.enable = true;
-          }
+              security.sudo.enable = true;
+            }
+          )
         ];
       };
     };
